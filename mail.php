a51c141e1fc34680556aaaab1375eade510af52d
p.venhryn <p.venhryn@pozitiff.ua>
2020-02-06 18:30:30 +0200

    'update_file'

--- mail.php
+++ mail.php
@@ -14,23 +14,23 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'venhrunpetro@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'V@24429612'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Password = '24429612'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl, tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('venhrunpetro@gmail.com'); // от кого будет уходить письмо?
-$mail->addAddress('p.venhry@pozitiff.ua');     // Кому будет уходить письмо 
+$mail->setFrom('p.venhry@pozitiff.ua'); // от кого будет уходить письмо?
+$mail->addAddress('venhrunpetro@gmail.com');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
 //$mail->addBCC('bcc@example.com');
 //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+$mail->isHTML(true);                              // Set email format to HTML
 
 $mail->Subject = 'Заявка с тестового сайта';
 $mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
-$mail->AltBody = '';
+$mail->AltBody = 'sadsad';
 
 if(!$mail->send()) {
     echo 'Error';
